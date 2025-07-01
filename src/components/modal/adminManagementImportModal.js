622190689023d905c7320fdbd08bcb898e8955e5
JSAIVISHNU2401 <saivishnu.j@nichi.com>
2025-07-01 15:37:17 +0530

    adminManagementImportModal accessibility imrpvements

--- src/components/modal/adminManagementImportModal.js
+++ src/components/modal/adminManagementImportModal.js
@@ -1,134 +1,152 @@
-import React, { useContext, useRef } from "react"
-import { Dialog } from 'primereact/dialog';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import React, { useContext, useRef } from "react";
+import { Dialog } from "primereact/dialog";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-import { Button, InputFile, ValidationError } from "@/components"; 
+import { Button, InputFile, ValidationError } from "@/components";
 import { getValueByKeyRecursively as translate } from "@/helper";
 import { LayoutContext } from "@/layout/context/layoutcontext";
 
 export default function AdminManagementImportModal(props) {
-    const { localeJson } = useContext(LayoutContext);
-    const { open, close, importFile, modalHeaderText } = props && props;
+  const { localeJson } = useContext(LayoutContext);
+  const { open, close, importFile, modalHeaderText } = props && props;
 
-    const fileInputRef = useRef(null);
-    const initialValues = { file: null };
+  const fileInputRef = useRef(null);
+  const initialValues = { file: null };
 
-    const schema = Yup.object().shape({
-        file: Yup.mixed()
-            .required(translate(localeJson, 'file_csv_required'))
-            .test('is-csv', translate(localeJson, 'select_csv_format'), (value) => {
-                if (!value) return true; // If no file is selected, the validation passes.
+  const schema = Yup.object().shape({
+    file: Yup.mixed()
+      .required(translate(localeJson, "file_csv_required"))
+      .test("is-csv", translate(localeJson, "select_csv_format"), (value) => {
+        if (!value) return true; // If no file is selected, the validation passes.
 
-                const allowedExtensions = ['csv']; // Define the allowed file extensions (in this case, just 'csv').
-                const fileExtension = value.name.split('.').pop(); // Get the file extension from the file name.
+        const allowedExtensions = ["csv"]; // Define the allowed file extensions (in this case, just 'csv').
+        const fileExtension = value.name.split(".").pop(); // Get the file extension from the file name.
 
-                // Check if the file extension is in the list of allowed extensions.
-                return allowedExtensions.includes(fileExtension.toLowerCase());
-            }),
-    });
+        // Check if the file extension is in the list of allowed extensions.
+        return allowedExtensions.includes(fileExtension.toLowerCase());
+      }),
+  });
 
-    const resetAndCloseForm = (callback) => {
-        close();
-        callback();
-    }
+  const resetAndCloseForm = (callback) => {
+    close();
+    callback();
+  };
 
-    return (
-        <>
-            <Formik
-                validationSchema={schema}
-                initialValues={initialValues}
-                onSubmit={(values, { resetForm }) => {
-                    importFile(values.file)
-                    close();
-                    resetAndCloseForm(resetForm);
+  return (
+    <>
+      <Formik
+        validationSchema={schema}
+        initialValues={initialValues}
+        onSubmit={(values, { resetForm }) => {
+          importFile(values.file);
+          close();
+          resetAndCloseForm(resetForm);
+        }}
+      >
+        {({ errors, touched, setFieldValue, handleSubmit, resetForm }) => (
+          <div className="">
+            <form onSubmit={handleSubmit}>
+              <Dialog
+                className={`new-custom-modal`}
+                header={<div>{modalHeaderText}</div>}
+                visible={open}
+                draggable={false}
+                blockScroll={true}
+                onHide={() => {
+                  resetAndCloseForm(resetForm);
+                  close();
                 }}
-            >
-                {({
-                    errors,
-                    touched,
-                    setFieldValue,
-                    handleSubmit,
-                    resetForm
-                }) => (
-                    <div className="">
-                        <form onSubmit={handleSubmit}>
-                            <Dialog
-                                className={`new-custom-modal`}
-                                header={
-                                    <div>
-                                        {modalHeaderText}
-                                    </div>
-                                }
-                                visible={open}
-                                draggable={false}
-                                blockScroll={true}
-                                onHide={() => {
-                                    resetAndCloseForm(resetForm);
-                                    close();
-                                }}
-                                footer={
-                                    <div className="text-center">
-                                        <Button buttonProps={{
-                                            type: "submit",
-                                            buttonClass: "w-8rem update-button",
-                                            text: translate(localeJson, 'import'),
-                                            severity: "primary",
-                                            onClick: () => {
-                                                handleSubmit();
-                                            },
-                                        }} parentClass={"inline update-button"} />
-                                    </div>
-                                }
-                            >
-                                <div className={`modal-content`}>
-                                    <div className="modal-header">
-                                        <div>
-                                            {modalHeaderText}
-                                        </div>
-                                    </div>
-                                    {props.tag && (<div className="mb-2">
-                                        <p>{translate(localeJson, 'hq_stockpile_export_title_modal')}</p>
-                                    </div>)}
-                                    <InputFile inputFileProps={{
-                                        onChange: (event) => {
-                                            setFieldValue("file", event.currentTarget.files[0]);
-                                        },
-                                        ref: fileInputRef,
-                                        placeholder: translate(localeJson, 'default_csv_file_placeholder')
-                                    }} parentClass={`bg-white w-full ${errors.file && touched.file && 'p-invalid '}`} />
-                                    <div className='pt-1'>
-                                        <ValidationError errorBlock={errors.file && touched.file && errors.file} />
-                                    </div>
-                                    <div className="text-center">
-                                        <div className="modal-button-footer-space">
-                                            <Button buttonProps={{
-                                                type: "submit",
-                                                buttonClass: "w-full update-button",
-                                                text: translate(localeJson, 'import'),
-                                                severity: "primary",
-                                                onClick: () => {
-                                                    handleSubmit();
-                                                },
-                                            }} parentClass={"update-button"} />
-                                        </div>
-                                        <div>
-                                            <Button buttonProps={{
-                                                buttonClass: "w-full back-button",
-                                                text: translate(localeJson, 'cancel'),
-                                                onClick: () => {
-                                                    resetAndCloseForm(resetForm);
-                                                    close();
-                                                },
-                                            }} parentClass={"back-button"} />
-                                        </div>
-                                    </div>
-                                </div>
-                            </Dialog>
-                        </form>
+                footer={
+                  <div className="text-center">
+                    <Button
+                      buttonProps={{
+                        type: "submit",
+                        buttonClass: "w-8rem update-button",
+                        text: translate(localeJson, "import"),
+                        severity: "primary",
+                        onClick: () => {
+                          handleSubmit();
+                        },
+                      }}
+                      parentClass={"inline update-button"}
+                    />
+                  </div>
+                }
+              >
+                <div className={`modal-content`}>
+                  <div className="modal-header">
+                    <div>{modalHeaderText}</div>
+                  </div>
+                  {props.tag && (
+                    <div className="mb-2">
+                      <p>
+                        {translate(
+                          localeJson,
+                          "hq_stockpile_export_title_modal"
+                        )}
+                      </p>
                     </div>
-                )}
-            </Formik>
-        </>
-    );
+                  )}
+                  <label htmlFor="admin_import_csv_file" className="sr-only">
+                    {translate(localeJson, "default_csv_file_placeholder")}
+                  </label>
+                  <InputFile
+                    inputFileProps={{
+                      id: "admin_import_csv_file",
+                      onChange: (event) => {
+                        setFieldValue("file", event.currentTarget.files[0]);
+                      },
+                      ref: fileInputRef,
+                      placeholder: translate(
+                        localeJson,
+                        "default_csv_file_placeholder"
+                      ),
+                    }}
+                    parentClass={`bg-white w-full ${
+                      errors.file && touched.file && "p-invalid "
+                    }`}
+                  />
+                  <div className="pt-1">
+                    <ValidationError
+                      errorBlock={errors.file && touched.file && errors.file}
+                    />
+                  </div>
+                  <div className="text-center">
+                    <div className="modal-button-footer-space">
+                      <Button
+                        buttonProps={{
+                          type: "submit",
+                          buttonClass: "w-full update-button",
+                          text: translate(localeJson, "import"),
+                          severity: "primary",
+                          onClick: () => {
+                            handleSubmit();
+                          },
+                        }}
+                        parentClass={"update-button"}
+                      />
+                    </div>
+                    <div>
+                      <Button
+                        buttonProps={{
+                          buttonClass: "w-full back-button",
+                          text: translate(localeJson, "cancel"),
+                          onClick: () => {
+                            resetAndCloseForm(resetForm);
+                            close();
+                          },
+                        }}
+                        parentClass={"back-button"}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Dialog>
+            </form>
+          </div>
+        )}
+      </Formik>
+    </>
+  );
 }
